9db81cdf9eaf90fa7101aa4e8c8928b4da9da29d
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-15 21:24:32 +0700

    tambah logo, brand name dan favincon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,11 +31,16 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandName('Sistem Booking Biliar')
+            ->brandLogo($this->getBrandLogo())
+            ->brandLogoHeight('8rem')
+            ->favicon(asset('images/logo_biliar.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
                 Dashboard::class,
             ])
+            ->darkMode(false)
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
@@ -55,5 +60,14 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ]);
+    }
+
+    protected function getBrandLogo(): ?string
+    {
+        if (request()->is('admin/login')) {
+            return asset('images/logo_biliar.png');
+        }
+
+        return null;
     }
 }
