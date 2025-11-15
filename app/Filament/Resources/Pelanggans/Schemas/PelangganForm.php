2c55971488573d40ed8e69490772694a73e6d9a9
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-15 21:48:34 +0700

    buat resource pelanggan

---
+++ app/Filament/Resources/Pelanggans/Schemas/PelangganForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\Pelanggans\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class PelangganForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('nama')
+                    ->required(),
+                TextInput::make('nomor_wa')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email(),
+                Toggle::make('active')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->numeric(),
+                TextInput::make('updated_by')
+                    ->numeric(),
+                TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+}
