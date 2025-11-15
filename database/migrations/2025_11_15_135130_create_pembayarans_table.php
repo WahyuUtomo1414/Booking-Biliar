b43d553403dfeaa8bf2a2da3cbe116e9ca19f136
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-15 20:52:44 +0700

    table pembayaran

---
+++ database/migrations/2025_11_15_135130_create_pembayarans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Traits\BaseModelSoftDelete;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained('booking');
+            $table->double('jumlah_bayar');
+            $table->string('jenis_pembayaran', 128);
+            $table->string('bukti_pembayaran', 128)->nullable();
+            $table->string('status', 128)->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+};
