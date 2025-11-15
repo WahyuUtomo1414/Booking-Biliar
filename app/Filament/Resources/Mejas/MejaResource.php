8b621eaa2363358093a62b2120ac5f8ae480b478
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-15 21:45:32 +0700

    ubah nav label dan buat nav group master

--- app/Filament/Resources/Mejas/MejaResource.php
+++ app/Filament/Resources/Mejas/MejaResource.php
@@ -15,12 +15,19 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use UnitEnum;
 
 class MejaResource extends Resource
 {
     protected static ?string $model = Meja::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::TableCells;
+
+    protected static string | UnitEnum | null $navigationGroup = 'Data Master';
+
+    protected static ?string $navigationLabel = 'Meja';
+
+    protected static ?string $pluralModelLabel = 'Meja';
 
     public static function form(Schema $schema): Schema
     {
