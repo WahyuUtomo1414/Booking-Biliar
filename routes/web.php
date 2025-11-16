52ee5bfc8a14a32d9bb3732ebf88f7ec0a3dab3a
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-16 12:08:26 +0700

    buat route print pdf

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingPrintController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -9,3 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/login');
 });
+
+Route::get('/booking/{id}/print', [BookingPrintController::class, 'print'])
+    ->name('booking.print');
